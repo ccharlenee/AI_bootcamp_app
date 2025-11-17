b7a4f2a355c54f9eb6738135afe5a6c77bae06af
ccharlenee <36877579+ccharlenee@users.noreply.github.com>
2025-11-17 23:44:16 +0800

    password

--- pages/3_About_Us.py
+++ pages/3_About_Us.py
@@ -7,11 +7,13 @@ st.set_page_config(
 )
 # endregion <--------- Streamlit App Configuration --------->
 
-st.title("About this App")
+st.title("About Us")
 
-st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
+st.write("This is an app that allows users to facilitate search of NParks publication database to get a quick understanding of what has been done before")
+st.write("It is built using Streamlit and Langchain with GPT-4o-mini as the LLM")
+st.write("Developed by Charlene Ng and Cheong Limin as part of the AI Bootcamp Project")
 
 with st.expander("How to use this App"):
-    st.write("1. Enter your prompt in the text area.")
+    st.write("1. Enter your query in the text area.")
     st.write("2. Click the 'Submit' button.")
     st.write("3. The app will generate a text completion based on your prompt.")
