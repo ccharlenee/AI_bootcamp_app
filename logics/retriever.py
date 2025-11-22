6db2560673d138d2473fa9300138f0d8d75d5257
Li Min Cheong <limin@Lis-MacBook-Air-2.local>
2025-11-23 00:59:25 +0800

    changed clone_dir

--- logics/retriever.py
+++ logics/retriever.py
@@ -5,7 +5,7 @@ from langchain_core.runnables import RunnableLambda
 from logics.config import DB_DIR, repo_url, clone_dir, get_embeddings
 
 def clone_repo():
-    if not os.path.exists(CLONE_DIR):
+    if not os.path.exists(clone_dir):
         subprocess.run(["git", "clone", repo_url, clone_dir])
 
 def load_retriever(k=8):
