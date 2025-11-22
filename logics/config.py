8ace533eda1dba4f4565e4277b5d6909c35cce10
Li Min Cheong <limin@Lis-MacBook-Air-2.local>
2025-11-23 02:50:18 +0800

    update to db3

--- logics/config.py
+++ logics/config.py
@@ -8,7 +8,7 @@ clone_dir = os.path.join(os.getcwd(), "repo_clone")
 if not os.path.exists(clone_dir):
     subprocess.run(["git", "clone", repo_url, clone_dir])
 
-DB_DIR = os.path.join(clone_dir, "db2")
+DB_DIR = os.path.join(clone_dir, "db3")
 
 PDF_DIR = "/Users/limin/Documents/Python/Journal_database"  # folder containing many PDFs
 
