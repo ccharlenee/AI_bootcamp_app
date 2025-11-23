ed89f994a142dd0930830ad57d8d52ced5ff51c5
Li Min Cheong <limin@Lis-MacBook-Air-2.local>
2025-11-23 13:49:20 +0800

    added storage from google cloud

--- logics/Ingest.py
+++ logics/Ingest.py
@@ -1,6 +1,7 @@
 import os
 import io
 import streamlit as st
+from google.cloud import storage
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
