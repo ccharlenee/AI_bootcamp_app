e20ff3eaec829e949e22cfba563a28420becc259
Li Min Cheong <limin@Lis-MacBook-Air-2.local>
2025-11-23 15:27:33 +0800

    workaround to load image

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -2,6 +2,7 @@ import streamlit as st
 from helper_functions.style import apply_custom_styles
 from helper_functions.utility import check_password
 from helper_functions.utility import login
+from pil import Image
 
 apply_custom_styles()
 
@@ -23,6 +24,15 @@ st.title("Methodology")
 
 st.write("Data sources: Journal articles are loaded on Google Cloud Bucket")
 st.write("Methodology is as follows:")
-image_url = "https://storage.cloud.google.com/research_app_method/Methodology.drawio.png"
-image = image.open(image_url)
-st.image(image, use_container_width=True)
+
+blob = bucket.blob(https://storage.cloud.google.com/research_app_method/Methodology.drawio.png)
+image_data = blob.download_as_bytes()
+img = Image.open(io.BytesIO(image_data))
+
+buffered = io.BytesIO()
+img.save(buffered, format="PNG")
+img_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+img_src = f"data:image/png;base64,{img_base64}"
+html_code = f'<img src="{img_src}" width="200" style="border-radius:10px;"/>'
+st.markdown(html_code, unsafe_allow_html=True)
+#st.image(image, use_container_width=True)
