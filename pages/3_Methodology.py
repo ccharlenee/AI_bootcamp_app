ca152197321f9401d8669390031ce441b6df2752
Li Min Cheong <limin@Lis-MacBook-Air-2.local>
2025-11-23 16:31:06 +0800

    change image url

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -2,7 +2,6 @@ import streamlit as st
 from helper_functions.style import apply_custom_styles
 from helper_functions.utility import check_password
 from helper_functions.utility import login
-from pil import Image
 
 apply_custom_styles()
 
