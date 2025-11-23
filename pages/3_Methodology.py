bd0ad6b9c398c8c228c812480201c7c7b1573a7b
Li Min Cheong <limin@Lis-MacBook-Air-2.local>
2025-11-23 14:23:27 +0800

    updated methodology

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -21,6 +21,9 @@ elif st.session_state["auth"] == "user":
 #start write up here
 st.title("Methodology")
 
-st.write("Methodology is as follows : xxxx")
-st.write("Data sources: Journal articles are loaded ")
-st.write("xxx")
+st.write("Data sources: Journal articles are loaded on Google Cloud Bucket")
+st.write("Methodology is as follows:")
+st.write("Journal articles are ingested into Chromadatabase using PyPDF and text are split using RecursiveCharacterTextSplitter.")
+st.write("A Chroma vector store of embeddings are generated for efficient retrieval.")
+st.write("The Chroma vector store is converted into a retriever object to be queried.")
+st.write("The prompt and additional context from users uses LLMs to generate a policy related summary for Singapore.")
