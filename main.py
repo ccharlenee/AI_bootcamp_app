0e7cb7d10cebcd081580cf13fd0c7472e72c0bb6
Li Min Cheong <limin@Lis-MacBook-Air-2.local>
2025-11-23 16:56:13 +0800

    add disclaimer formatting

--- main.py
+++ main.py
@@ -48,6 +48,18 @@ with st.form(key="form"):
     user_prompt = st.text_area("Enter your prompt here", height=200)
     submit_button = st.form_submit_button("Generate summaries")
 
+st.title("Disclaimer")
+
+with st.expander ("Click to expand"):
+    st.write("""
+
+        IMPORTANT NOTICE: This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+        Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+        Always consult with qualified professionals for accurate and personalized advice.
+     """)
+    
 if submit_button:
     query = user_prompt
 
@@ -89,15 +101,6 @@ if submit_button:
             "question": query
         })
         st.write(summary)
-st.title("Disclaimer")
 
-with st.expander ("Click to expand"):
-    st.write("""
 
-        IMPORTANT NOTICE: This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
-
-        Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
-
-        Always consult with qualified professionals for accurate and personalized advice.
-     """)
 
